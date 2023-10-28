fab8594df30193110b8ea41201f20d75cda80edc
djkeu <mrkooij@gmail.com>
2023-10-28 11:25:22 +0200

    Store response in var

--- python_repos.py
+++ python_repos.py
@@ -8,3 +8,7 @@ url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = { 'Accept': 'application/vnd.github.v3+json' }
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
+
+# Store API response in a var
+response_dict = r.json()
+
