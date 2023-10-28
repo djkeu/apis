426d02d6454463257edc57660d25fde33f10d0b0
djkeu <mrkooij@gmail.com>
2023-10-28 12:41:11 +0200

    Make API call, process results

--- python_visual_repos.py
+++ python_visual_repos.py
@@ -2,3 +2,19 @@ import requests
 
 from plotly.graph_objs import Bar
 from plotly import offline
+
+# Make an API call, store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = { 'Accept': 'application/vnd.github.v3+json' }
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Process results
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names, stars = [], []
+
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
