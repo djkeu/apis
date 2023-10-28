dd98772c6d636ad501c1b6867dd8f119b740831f
djkeu <mrkooij@gmail.com>
2023-10-28 19:43:24 +0200

    Make API call, store response

--- hn_article.py
+++ hn_article.py
@@ -1,3 +1,8 @@
 import requests
 import json
 
+# Make an API call, store the response
+url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
