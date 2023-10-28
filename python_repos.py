db3049367e144bae4881f642c06080b5cf55258b
djkeu <mrkooij@gmail.com>
2023-10-28 11:43:51 +0200

    Print other info first repo

--- python_repos.py
+++ python_repos.py
@@ -26,6 +26,12 @@ for key in sorted(repo_dict.keys()):
     print(key)
 """
 
-print("Selected information about first repository")
+print("\nSelected information about first repository:")
 print(f"Name: {repo_dict['name']}")
 print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
+
