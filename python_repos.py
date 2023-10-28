ddab6c6fe3f5fb86b4c8415671e4842a05f012e6
djkeu <mrkooij@gmail.com>
2023-10-28 11:38:56 +0200

    Print name, owner of repo_dict

--- python_repos.py
+++ python_repos.py
@@ -17,5 +17,15 @@ print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# Process results
-print(response_dict.keys())
+# Examine the first repo
+repo_dict = repo_dicts[0]
+
+"""
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
+"""
+
+print("Selected information about first repository")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
