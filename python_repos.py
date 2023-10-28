4214c6341bf75d4d96b08f3303fe25e7b0b20911
djkeu <mrkooij@gmail.com>
2023-10-28 11:24:25 +0200

    Make API call

--- python_repos.py
+++ python_repos.py
@@ -2,3 +2,9 @@
 
 import requests
 
+# Make an API call, store the request
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = { 'Accept': 'application/vnd.github.v3+json' }
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
