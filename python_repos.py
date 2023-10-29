05f15f6ebf9137eb05a47af9f0e3e37194e700b3
djkeu <mrkooij@gmail.com>
2023-10-29 15:10:02 +0100

    17.1 Other languages

--- python_repos.py
+++ python_repos.py
@@ -5,6 +5,7 @@ import requests
 # Make an API call, store the request
 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+# url = 'https://api.github.com/search/repositories?q=language:c&sort=stars'
 headers = { 'Accept': 'application/vnd.github.v3+json' }
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
