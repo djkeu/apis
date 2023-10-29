f1b54868ff53f5114d55577534c4e81adbd64b66
djkeu <mrkooij@gmail.com>
2023-10-29 14:53:27 +0100

    Lower number of requests to prevent KeyError

--- hn_submissions.py
+++ hn_submissions.py
@@ -11,12 +11,15 @@ print(f"Status code: {r.status_code}")
 submission_ids = r.json()
 submission_dicts = []
 
-for submission_id in submission_ids[:30]:
+for submission_id in submission_ids[:5]:
     # Make a seperate API call for eacht submission
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
+
+    for key in response_dict.keys():
+        print(key)
 
     # Build a dictionary for each article
     submission_dict = {
