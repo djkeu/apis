e1f8fdc7be460d9e4f990f85330ca246637a00dc
djkeu <mrkooij@gmail.com>
2023-10-29 10:21:30 +0100

    Make api call, store the response

--- hn_submissions.py
+++ hn_submissions.py
@@ -1,1 +1,10 @@
-# p.373
+from operator import itemgetter
+
+import requests
+
+# Make an API call, store the response
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+
