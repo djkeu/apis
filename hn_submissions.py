c5fd86d7de725d6abd0728ebb90063cd9b464683
djkeu <mrkooij@gmail.com>
2023-10-29 10:48:51 +0100

    Add Note: KeuError 'descendants'

--- hn_submissions.py
+++ hn_submissions.py
@@ -22,9 +22,14 @@ for submission_id in submission_ids[:30]:
     submission_dict = {
         'title': response_dict['title'],
         'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+        # Note: KeyError 'descendants'. 
         'comments': response_dict['descendants'],
     }
     submission_dicts.append(submission_dict)
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
 
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Discussion link: {submission_dict['hn_link']}")
+    print(f"Comments: {submission_dict['comments']}")
