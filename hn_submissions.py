0955ba4dc3bd110ae6ceb4f3f4be16e3a26d0f5a
djkeu <mrkooij@gmail.com>
2023-10-29 10:30:46 +0100

    Build a dict for each article

--- hn_submissions.py
+++ hn_submissions.py
@@ -17,4 +17,12 @@ for submission_id in submission_ids[:30]:
     r = requests.get(url)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
+
+    # Build a dictionary for each article
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+        'comments': response_dict['descendants'],
+    }
+    submission_dicts.append(submission_dict)
     
