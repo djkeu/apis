1a665993978c39fd6d9964f3e35ee548295f36e3
djkeu <mrkooij@gmail.com>
2023-10-30 09:27:34 +0100

    Fill submission_names, .._comments with data

--- hn_visual_submission.py
+++ hn_visual_submission.py
@@ -2,7 +2,7 @@ from operator import itemgetter
 
 import requests
 
-from plotly.subplots import Bar
+from plotly.graph_objs import Bar
 from plotly import offline
 
 # Make an API call, store the response
@@ -12,15 +12,16 @@ print(f"Status code: {r.status_code}")
 
 # Process information about each submission
 submission_ids = r.json()
-submission_dicts = submission_ids['items'] 
+# submission_dicts = submission_ids['items'] 
+submission_dicts = [] 
+
 submission_names, submission_comments = [], []
 
-# Note: KeyError 'descendants' if submission_ids[] >= [:10] 
-for submission_id in submission_ids[:5]:
-    # Make a seperate API call for eacht submission
+for submission_id in submission_ids[:9]:
+    # Make a seperate API call for each submission
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
-    print(f"id: {submission_id}\tstatus: {r.status_code}")
+    # print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
 
     # Build a dictionary for each article
@@ -31,28 +32,31 @@ for submission_id in submission_ids[:5]:
     }
     submission_dicts.append(submission_dict)
 
+    submission_name = submission_dict['title']
+    submission_names.append(submission_name)
+    # print(submission_names)
 
+    submission_comment = submission_dict['comments']
+    submission_comments.append(submission_comment)
+    # print(submission_comments)
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
-
-"""
-for submission_dict in submission_dicts:
-    print(f"\nTitle: {submission_dict['title']}")
-    print(f"Discussion link: {submission_dict['hn_link']}")
-    print(f"Comments: {submission_dict['comments']}")
-"""
-
 
 # Try it yourself 17.2 Active discussions
 data = [{
     'type': 'bar',
-    'x': submission_dict['title'],
-    'y': submission_dict['comments'], 
+    # 'x': submission_dict['title'],
+    # 'y': submission_dict['comments'],
+    # ToDo: fill submission_names, submission_comments
+    'x': submission_names,
+    'y': submission_comments,
+    'marker': {'color': 'rgb(160, 60, 60)'}, 
 }]
 
 my_layout = {
     'title': "HackerNews submissions",
-
+    'xaxis': {'title': 'Repository'},
+    'yaxis': {'title': 'Comments'}
 }
 
 fig = {'data': data, 'layout': my_layout}
