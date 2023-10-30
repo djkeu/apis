44bc6a15ed90e0c76cbb08a29d30c309c9c59d1b
djkeu <mrkooij@gmail.com>
2023-10-30 09:29:52 +0100

    Remove commented out code

--- hn_visual_submission.py
+++ hn_visual_submission.py
@@ -12,16 +12,12 @@ print(f"Status code: {r.status_code}")
 
 # Process information about each submission
 submission_ids = r.json()
-# submission_dicts = submission_ids['items'] 
-submission_dicts = [] 
-
-submission_names, submission_comments = [], []
+submission_dicts, submission_names, submission_comments = [], [], []
 
 for submission_id in submission_ids[:9]:
     # Make a seperate API call for each submission
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
-    # print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
 
     # Build a dictionary for each article
@@ -34,23 +30,18 @@ for submission_id in submission_ids[:9]:
 
     submission_name = submission_dict['title']
     submission_names.append(submission_name)
-    # print(submission_names)
 
     submission_comment = submission_dict['comments']
     submission_comments.append(submission_comment)
-    # print(submission_comments)
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
 
 # Try it yourself 17.2 Active discussions
 data = [{
     'type': 'bar',
-    # 'x': submission_dict['title'],
-    # 'y': submission_dict['comments'],
-    # ToDo: fill submission_names, submission_comments
     'x': submission_names,
     'y': submission_comments,
-    'marker': {'color': 'rgb(160, 60, 60)'}, 
+    'marker': {'color': 'rgb(200, 100, 100)'}, 
 }]
 
 my_layout = {
